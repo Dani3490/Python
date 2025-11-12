b225e2e413eaab6ceffb3974fff039ff4bd52483
Dani3490 <dmanteca21216@iesjoanramis.org>
2025-11-12 13:39:49 +0100

    Ex 1

--- Prova1.py
+++ Prova1.py
@@ -3,5 +3,15 @@ b = float(input("Escriure el segon nombre: "))
 d = float(input("Escriure el tercer nombre: "))
 c = a + b + d
 print("El resultat de la suma {} + {} + {} és {}".format(a, b, d, c))
+if c>20:
+     print("El resultado es mayor a 20, que és {} ".format(c))
+else: 
+     print("El resultado es menor a 20, que és {} ".format (c))
+
 c = a * b * d
 print("El resultat de la multiplicació {} * {} * {} és {}".format(a, b, d, c))
+if c>100:
+     print("El resultado es mayor a 100, que és {} ".format (c))
+else: 
+     print("El resultado es menor a 100, que és {} ".format (c))
+
