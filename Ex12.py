d91a5de7228f9b972d155ca86bb059efbbb765d2
Dani3490 <dmanteca21216@iesjoanramis.org>
2025-11-13 11:56:38 +0100

    Ex12c

--- Ex12.py
+++ Ex12.py
@@ -1,7 +1,15 @@
+def majoredat(a):
+     if a<18:
+          print("Eres menor de edad, no puedes acceder")
+     elif a>18:  
+          print("Eres mayor de edad, puedes acceder")     
+     else: 
+          print("Tienes justo 18, vas pelao, pero puedes acceder")
+
+
 a = int(input("Pon tu edad: "))
-if a<18:
-     print("Eres menor de edad, no puedes acceder")
-elif a>18:  
-     print("Eres mayor de edad, puedes acceder")     
-else: 
-     print("Tienes justo 18, vas pelao, pero puedes acceder")
+majoredat (a)
+a = int(input("Pon de nuevo tu edad: "))
+majoredat (a)
+
+
