fe182aa2aa9c4003657d6517408731f1d1a8103c
Dani3490 <dmanteca21216@iesjoanramis.org>
2025-11-13 11:36:56 +0100

    Ex12

---
+++ Ex12.py
@@ -0,0 +1,8 @@
+a = input("Pon tu edad: ")
+print("Tienes {} años".format(a))
+if a>18:
+     print("Eres menor de edad, no puedes acceder")
+else: 
+     print("Eres mayor de edad, puedes acceder")
+if a=18: 
+     print("Tienes justo 18, vas pelao, pero puedes acceder")
