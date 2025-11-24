637c2c1d293ec1dd01303a60a6dd131b7e4cde61
Dani3490 <dmanteca21216@iesjoanramis.org>
2025-11-24 09:43:14 +0100

    Ex17.b

--- Ex17.py
+++ Ex17.py
@@ -8,5 +8,9 @@ def ex17(a):
     return len(a)
 
 # Programa principal
-a=[1, 3, 4, 5]
+a=[1, 3, 4, "Pedro", [3,6], "a", [1, [3,4],5]]
 print(ex17(a))
+b = "Som en Joan Ramis, i tú què tal estàs?"
+print(ex17(b))
+c=[]
+print(ex17(c))
