273ebe89d19af5d34274089665a4c4d02136afc5
Dani3490 <dmanteca21216@iesjoanramis.org>
2025-11-25 10:07:05 +0100

    Ex23

---
+++ Ex23.py
@@ -0,0 +1,9 @@
+def crear_repetits(num, caracter):
+    return caracter * num
+
+
+# Proves
+if __name__ == "__main__":
+    print(crear_repetits(5, "a"))   
+    print(crear_repetits(3, "x"))   
+    print(crear_repetits(7, "*"))   
