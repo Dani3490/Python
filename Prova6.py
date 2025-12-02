2cd1053915cc5e920ab0afec544e1eb55da6b9cb
Dani3490 <dmanteca21216@iesjoanramis.org>
2025-12-02 10:00:53 +0100

    prova6

---
+++ Prova6.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+"""
+def ordenar(x, y):
+
+    if x>y:
+        return y, x
+    elif y>x:
+        return x, y
+    else:
+        return x, y
+ 
+v1 = int(input("Intro el 1r número: "))
+v2 = int(input("Intro el 2n número: "))
+v2, v1 = ordenar(v1, v2)
+for e in range(v2, v1+1, 2):
+    if e%2==1:
+        print(e)
+
+v1 = int(input("Intro el 1r número: "))
+v2 = int(input("Intro el 2n número: "))
+r = (v1*v2) // 2
+for i in range(r, -1, -1):
+    print(i)"""
