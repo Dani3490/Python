439120fcec901e0d1a42bc46968cf75769fabd65
Dani3490 <dmanteca21216@iesjoanramis.org>
2025-12-02 10:00:10 +0100

    Ex30

---
+++ Ex30.py
@@ -0,0 +1,7 @@
+def binari_a_enter(binari):
+    return int(binari, 2)
+
+print(binari_a_enter("1010"))      
+print(binari_a_enter("1111"))     
+print(binari_a_enter("10000"))     
+print(binari_a_enter("11111111")) 
