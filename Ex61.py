f49bda1549337f9188b1b5094462929f66820234
Dani3490 <dmanteca21216@iesjoanramis.org>
2025-12-09 09:12:10 +0100

    Ex61

--- Ex61.py
+++ Ex61.py
@@ -1,1 +1,17 @@
-print("Prova")
+def lenp(frase):
+    paraules = frase.split()
+    return list(map(len, paraules))
+
+
+# Exemples d'ús
+print(lenp("Hola món com estàs"))
+# Sortida: [4, 3, 3, 5]
+
+print(lenp("Python és genial"))
+# Sortida: [6, 2, 6]
+
+print(lenp("Aquesta és una prova"))
+# Sortida: [7, 2, 3, 5]
+
+print(lenp("a"))
+# Sortida: [1]
